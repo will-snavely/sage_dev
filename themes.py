1b2e3243328f5d9773ca3f39344bed140ca7633d
Will Snavely <thoreandan@bilbo.baggins>
2025-09-02 22:34:41 +0000

    cleaning up themes.py

--- themes.py
+++ themes.py
@@ -5,58 +5,57 @@ import os
 import sys
 import json
 import logging
+import shlex
+
+WORDPRESS_ROOT = "/srv/www/wordpress/"
+logger = logging.getLogger(__name__)
+
+def docker_cmd(cmd, service, user, workdir):
+    result = ["docker-compose", "exec"]
+    result += ["--user", user] if user else []
+    result += ["--workdir", workdir] if workdir else [] 
+    result += [service] 
+    result += shlex.split(cmd) if isinstance(cmd, str) else list(cmd)
+    return result
+
+def log_docker_cmd(cmd, level=logging.DEBUG, *args, **kwargs):
+    msg = "(docker call) " + shlex.join(cmd)
+    logger.log(level, msg)
+
+def run_docker_cmd(cmd, service="web", user="www-data", workdir=WORDPRESS_ROOT):
+    dcmd = docker_cmd(cmd, service=service, user=user, workdir=workdir)
+    log_docker_cmd(dcmd)
+    return subprocess.run(dcmd)
 
 def list_themes(args):
-    command = [
-        "docker-compose", "exec",
-        "--workdir", "/srv/www/wordpress",
-        "--user", "www-data",
-        "web",
-        "wp", "theme", "list"]
-    print(" ".join(command))
-    result = subprocess.run(command)
+    run_docker_cmd("wp theme list")
+
+def activate_theme(args):
+    run_docker_cmd(f"wp theme activate {args.name}")
 
 def build_sage_theme(args):
-    name = args.name
-    theme_dir = f"/srv/www/wordpress/wp-content/themes/{name}"
-    base_cmd =  ["docker-compose", "exec", "--workdir", theme_dir, "web"]
-
-    npm_install = base_cmd + ["bash", "-c", "npm install"]
-    logging.info(" ".join(npm_install))
-    result = subprocess.run(npm_install)
-
-    npm_build = base_cmd + ["npm", "run", "build"]
-    logging.info(" ".join(npm_build))
-    result = subprocess.run(npm_build)
+    theme_dir = f"/srv/www/wordpress/wp-content/themes/{args.name}"
+    run_docker_cmd("npm install", workdir=theme_dir, user="root")
+    run_docker_cmd("npm run build", workdir=theme_dir, user="root")
 
 def create_sage_theme(args):
-    cwd = os.getcwd()
     name = args.name
-    host_theme_dir = os.path.join(cwd, "themes", name)
-    web_themes_dir = "/srv/www/wordpress/wp-content/themes"
-    os.makedirs(host_theme_dir, exist_ok=True)
-    base_cmd =  ["docker-compose", "exec", "--workdir", web_themes_dir, "web"]
-
-    sage_cmd = base_cmd + ["composer", "create-project", "roots/sage", name]
-    logging.info(" ".join(sage_cmd))
-    result = subprocess.run(sage_cmd)
-
-    chown_cmd = base_cmd + ["chown", "-R", "www-data:www-data", name]
-    logging.info(" ".join(chown_cmd))
-    result = subprocess.run(chown_cmd)
-
-    chmod_cmd = base_cmd + ["chmod", "-R", "g+rw", name]
-    logging.info(" ".join(chmod_cmd))
-    result = subprocess.run(chmod_cmd)
+    themes_dir = "/srv/www/wordpress/wp-content/themes"
+    run_docker_cmd(f"composer create-project roots/sage {name}", 
+                   user="root", workdir=themes_dir)
+    run_docker_cmd(f"chown -R www-data:www-data {name}", 
+                   user="root", workdir=themes_dir)
+    run_docker_cmd(f"chmod -R g+rw {name}", 
+                   user="root", workdir=themes_dir)
 
     search = r"\/app\/themes\/sage\/public\/build\/"
     replace = r"\/wp-content\/themes\/" + name + r"\/public\/build\/"
-    sed_cmd = base_cmd + ["sed", "-i", f"s/{search}/{replace}/g", f"{name}/vite.config.js"]
-    print(" ".join(sed_cmd))
-    result = subprocess.run(sed_cmd)
+    run_docker_cmd(f"sed -i 's/{search}/{replace}/g' {name}/vite.config.js", workdir=themes_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+
+    parser.add_argument("-v", "--verbose", action="store_true")
 
     subparsers = parser.add_subparsers(title="Commands", help='Subcommand help.')
     parser_init = subparsers.add_parser("init", help="Create a new sage theme on the container.")
@@ -70,5 +69,10 @@ if __name__ == "__main__":
     parser_list = subparsers.add_parser("list", help="List themes installed on the container.")
     parser_list.set_defaults(func=list_themes)
 
+    parser_list = subparsers.add_parser("activate", help="Activate a theme on the container.")
+    parser_list.set_defaults(func=activate_theme)
+
     args = parser.parse_args()
+    log_level = logging.DEBUG if args.verbose else logging.INFO
+    logging.basicConfig(level=log_level)
     args.func(args)
