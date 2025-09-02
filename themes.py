59d1f384ea673b785b2139e99e944f399432a922
Will Snavely <thoreandan@bilbo.baggins>
2025-09-02 20:39:03 +0000

    Updating stuff

--- themes.py
+++ themes.py
@@ -4,12 +4,7 @@ import collections
 import os
 import sys
 import json
-
-def push_theme(args):
-    pass
-
-def pull_theme(args):
-    pass
+import logging
 
 def list_themes(args):
     command = [
@@ -21,40 +16,59 @@ def list_themes(args):
     print(" ".join(command))
     result = subprocess.run(command)
 
+def build_sage_theme(args):
+    name = args.name
+    theme_dir = f"/srv/www/wordpress/wp-content/themes/{name}"
+    base_cmd =  ["docker-compose", "exec", "--workdir", theme_dir, "web"]
+
+    npm_install = base_cmd + ["bash", "-c", "npm install"]
+    logging.info(" ".join(npm_install))
+    result = subprocess.run(npm_install)
+
+    npm_build = base_cmd + ["npm", "run", "build"]
+    logging.info(" ".join(npm_build))
+    result = subprocess.run(npm_build)
+
 def create_sage_theme(args):
     cwd = os.getcwd()
     name = args.name
     host_theme_dir = os.path.join(cwd, "themes", name)
     web_themes_dir = "/srv/www/wordpress/wp-content/themes"
     os.makedirs(host_theme_dir, exist_ok=True)
+    base_cmd =  ["docker-compose", "exec", "--workdir", web_themes_dir, "web"]
 
-    command = [
-        "docker-compose", "exec",
-        "--workdir", web_themes_dir,
-        "web",
-        "composer", "create-project", "roots/sage", name]
-    result = subprocess.run(command)
+    sage_cmd = base_cmd + ["composer", "create-project", "roots/sage", name]
+    logging.info(" ".join(sage_cmd))
+    result = subprocess.run(sage_cmd)
+
+    chown_cmd = base_cmd + ["chown", "-R", "www-data:www-data", name]
+    logging.info(" ".join(chown_cmd))
+    result = subprocess.run(chown_cmd)
+
+    chmod_cmd = base_cmd + ["chmod", "-R", "g+rw", name]
+    logging.info(" ".join(chmod_cmd))
+    result = subprocess.run(chmod_cmd)
+
+    search = r"\/app\/themes\/sage\/public\/build\/"
+    replace = r"\/wp-content\/themes\/" + name + r"\/public\/build\/"
+    sed_cmd = base_cmd + ["sed", "-i", f"s/{search}/{replace}/g", f"{name}/vite.config.js"]
+    print(" ".join(sed_cmd))
+    result = subprocess.run(sed_cmd)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     subparsers = parser.add_subparsers(title="Commands", help='Subcommand help.')
     parser_init = subparsers.add_parser("init", help="Create a new sage theme on the container.")
-    init_subparsers = parser_init.add_subparsers(title="Builder", help='Select a theme builder.')
-    parser_sage_init = init_subparsers.add_parser("sage", help="Create a blank Sage theme.")
-    parser_sage_init.add_argument("name", type=str, help="The name of the theme.")
-    parser_sage_init.set_defaults(func=create_sage_theme)
+    parser_init.add_argument("name", type=str, help="The name of the theme.")
+    parser_init.set_defaults(func=create_sage_theme)
 
-    parser_pull = subparsers.add_parser("pull", help="Pull a theme from the container.")
-    parser_pull.add_argument("name", type=str, help="The name of the theme.")
-    parser_pull.set_defaults(func=pull_theme)
+    parser_init = subparsers.add_parser("build", help="Build a sage theme on the container.")
+    parser_init.add_argument("name", type=str, help="The name of the theme.")
+    parser_init.set_defaults(func=build_sage_theme)
 
-    parser_push = subparsers.add_parser("push", help="Push a theme from the container.")
-    parser_push.add_argument("name", type=str, help="The name of the theme.")
-    parser_push.set_defaults(func=push_theme)
-
-    parser_push = subparsers.add_parser("list", help="List themes installed on the container.")
-    parser_push.set_defaults(func=list_themes)
+    parser_list = subparsers.add_parser("list", help="List themes installed on the container.")
+    parser_list.set_defaults(func=list_themes)
 
     args = parser.parse_args()
     args.func(args)
