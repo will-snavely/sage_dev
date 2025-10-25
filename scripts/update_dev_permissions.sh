75e221c93b9a1994688a7f3be62a074cc44ccfda
Will <will.snavely@gmail.com>
2025-10-24 22:17:06 -0400

    Updating permissions script

--- scripts/update_dev_permissions.sh
+++ scripts/update_dev_permissions.sh
@@ -1,5 +1,5 @@
 if ! getent group devs >/dev/null; then
     groupadd -g "$DEV_GROUP_ID" devs
-    usermod -aG devs www-data
-    chgrp -R /www devs
 fi
+usermod -aG devs www-data
+chgrp -R devs /www
