cd9e4a3f8e0cbe0a10af2f5825049b2d628072c4
EC2 Default User <ec2-user@ip-172-30-2-136.ec2.internal>
2026-01-17 09:50:35 +0000

    Refining https configuration

--- scripts/proxy/dev_entrypoint.sh
+++ scripts/proxy/dev_entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-cp /app/scripts/proxy/app.conf /etc/nginx/conf.d/app.conf
+rm -rf /etc/nginx/conf.d/default.conf
+
+if [ "$USE_HTTPS" = "1" ]; then
+    cp /app/scripts/proxy/app.https.conf /etc/nginx/conf.d/app.conf
+else
+    cp /app/scripts/proxy/app.http.conf /etc/nginx/conf.d/app.conf
+fi
+
 sed -i 's/WEB_HOSTNAME/'$WEB_HOSTNAME'/g' /etc/nginx/conf.d/app.conf
 sed -i 's/WEB_PORT/'$WEB_PORT'/g' /etc/nginx/conf.d/app.conf
-sed -i 's/PROXY_PORT/'$PROXY_PORT'/g' /etc/nginx/conf.d/app.conf
+sed -i 's/PROXY_PORT_HTTPS/'$PROXY_PORT_HTTPS'/g' /etc/nginx/conf.d/app.conf
+sed -i 's/PROXY_PORT_HTTP/'$PROXY_PORT_HTTP'/g' /etc/nginx/conf.d/app.conf
 nginx -c /etc/nginx/nginx.conf -g "daemon off;"
