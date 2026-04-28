d9f5e5413bfe56f29d8c722ef62dda38d4f6acc9
EC2 Default User <ec2-user@ip-172-30-2-159.ec2.internal>
2026-04-28 02:40:41 +0000

    working on prod docker

--- scripts/web/config_apache.sh
+++ scripts/web/config_apache.sh
@@ -1,6 +1,6 @@
 
 #!/bin/sh
-python3 /app/scripts/web/gen_apache_config.py $PROJECT_NAME
+python3 /app/scripts/web/gen_apache_config.py $PROJECT
 grep -q "Listen $WEB_PORT" /etc/apache2/ports.conf || echo "Listen $WEB_PORT" | tee -a /etc/apache2/ports.conf
 a2ensite wordpress
 a2enmod rewrite
