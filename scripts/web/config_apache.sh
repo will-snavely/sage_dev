ee431c470232b32ed5e23a317df981f613878bf3
EC2 Default User <ec2-user@ip-172-30-2-159.ec2.internal>
2026-04-28 02:18:10 +0000

    working on prod entrypoint

--- scripts/web/config_apache.sh
+++ scripts/web/config_apache.sh
@@ -1,9 +1,8 @@
+
 #!/bin/sh
-python3 /app/scripts/web/gen_bedrock_config.py $PROJECT_NAME
 python3 /app/scripts/web/gen_apache_config.py $PROJECT_NAME
 grep -q "Listen $WEB_PORT" /etc/apache2/ports.conf || echo "Listen $WEB_PORT" | tee -a /etc/apache2/ports.conf
 a2ensite wordpress
 a2enmod rewrite
 a2dissite 000-default
 service apache2 reload
-apache2ctl -D FOREGROUND
