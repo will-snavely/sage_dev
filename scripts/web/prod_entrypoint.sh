43d9b8e2b9e1bf1c16d144f82ceaa3a91ae17321
EC2 Default User <ec2-user@ip-172-30-2-159.ec2.internal>
2026-04-28 01:51:52 +0000

    Fixing prod entrypoint

--- scripts/web/prod_entrypoint.sh
+++ scripts/web/prod_entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 python3 /app/scripts/web/gen_bedrock_config.py $PROJECT_NAME
 python3 /app/scripts/web/gen_apache_config.py $PROJECT_NAME
+grep -q "Listen $WEB_PORT" /etc/apache2/ports.conf || echo "Listen $WEB_PORT" | tee -a /etc/apache2/ports.conf
 a2ensite wordpress
 a2enmod rewrite
 a2dissite 000-default
